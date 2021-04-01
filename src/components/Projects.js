ef813673c984258866b6b6a39bf7b52bdf9967be
Lek Jing Kai <lekjingkai@hotmail.com>
2021-04-01 19:06:28 +0800

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,11 @@ import "./Projects.css"
 import testimage3 from './assets/long-wallpaper.jpg'
 
 const Projects = () => {
-const descExample = "This is sample project made using sample tools (just a description)"
+
 
     return (
         <div className="projectContainer">
-            <ProjectCard projectLink={"https://www.google.com/"} imageSrc={testimage3} projTitle={"Project Title Placeholder"} projDesc={descExample}></ProjectCard>
+            <ProjectCard projectLink={"http://lekjingkai.github.io/travel"} imageSrc={testimage3} projTitle={"Travel Website (In Progress)"} projDesc={"A travel website made using React"}></ProjectCard>
         </div>
     )
 }
